d9da3aceefbe728efed016771636b079f61ff765
oldshi <30847960+sonehypnos@users.noreply.github.com>
2020-11-16 14:41:10 +0800

    Add files via upload

---
+++ week10.py
@@ -0,0 +1,38 @@
+# 1. 變數賦值
+
+## 用等號 = 讓電腦給記憶空間
+## 等號左邊
+## 等號右邊
+
+## 有甚麼用處
+
+# 2.數字資料 + 數學運算
+## 運算子 七種方式 // $ **
+## 函數
+
+# 3 電腦輸入
+## input() 函數
+
+# 4 電腦輸出
+## print()函數
+
+X=2
+Y=5
+print(X+Y)
+print(X-Y)
+print(X*Y)
+print(X/Y)
+print(X//Y)
+print(X%Y)
+
+magic_number = 22 ** 3 + 17 //5 +17 % 5
+print(magic_number)
+print("神奇數字為", magic_number)
+
+##input("Hint")
+num = int(input("Enter a number?"))
+num = num+1
+print(num)
+
+print(num=int(input("Enter a number?"))+1)
+      
---
+++ week8.py
@@ -0,0 +1,10 @@
+x = int(input("衣服數量"))
+x = x*300
+            
+y = int(input("褲子數量"))
+y = y*350
+            
+z = int(input("背心數量"))
+z = z*400
+           
+print(x+y+z)           
