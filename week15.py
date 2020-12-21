0be5bcfa66ca04ece8b71d305424ae482bd62989
oldshi <30847960+sonehypnos@users.noreply.github.com>
2020-12-21 14:53:34 +0800

    Add files via upload

---
+++ week15.py
@@ -0,0 +1,50 @@
+#import turtle
+#shelly = turtle.Turtle()
+#shelly.circle(120)
+
+
+#import turtle
+#shelly = turtle.Turtle()
+
+#shelly.hideturtle()
+
+#for i in range(4):
+#    shelly.forward(250)
+#   shelly.left(90)
+#    print(i)
+
+#import turtle
+#shelly = turtle.Turtle()
+
+#shelly.color("red")
+
+#for i in range(4):
+#    shelly.forward(250)
+#    shelly.left(90)
+#    print(i)
+
+#import turtle
+
+#shelly = turtle.Turtle()
+#colors = ["red","green","blue","black","purple","yellow"]
+
+#for n in range(6):
+#    shelly.color(colors[n])
+#    shelly.begin_fill()
+#    shelly.fillcolor(colors[n])
+
+#    for i in range(4):
+#        shelly.forward(250)
+#        shelly.left(90)
+#    shelly.right(60)
+#    shelly.end_fill()    
+
+import turtle
+shelly = turtle.Turtle()
+
+shelly.color("yellow")
+shelly.pensize(5)
+
+for i in range(5):
+    shelly.forward(250)
+    shelly.right(144)
